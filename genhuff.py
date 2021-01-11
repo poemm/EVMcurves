b27d7d6252eff6cab1afe9a7eb5b4cfd497533a7
poemm <36397285+poemm@users.noreply.github.com>
2021-01-11 08:34:53 -0600

    cleaned up and finished some tests

--- genhuff.py
+++ genhuff.py
@@ -123,7 +123,7 @@ def gen_equals(lhs,rhs,len_):
 ##########
 # buffers
 
-# memory offsets for local buffers used for temporary values, the first 576-bytes are zeros
+# memory offsets for inputs/outputs and for local temporary values
 buffer_offset = 0
 buffer_f12_function = buffer_offset
 buffer_offset += 12*48
@@ -512,7 +512,6 @@ def gen_f6mul(out,x,y,mod):
 
 def gen_f6sqr(out,x,mod):
   print("// f6sqr begin")
-  #gen_f6mul(out,x,x,mod)	# TODO: optimize
   x0 = x
   x1 = x0+96
   x2 = x1+96
@@ -1026,99 +1025,6 @@ def gen_f12sqrcyclotomic(out,x,mod):
 #######################
 # add two curve points
 
-def gen_Eadd__madd_2001_b(f,XYZout,XYZ1,XYZ2,mod):
-  # this is untested
-  print("/////////")
-  print("// Eadd https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#addition-add-2001-b")
-  # inputs/ouput
-  X1=XYZ1
-  Y1=X1+int(f[1])*48
-  Z1=Y1+int(f[1])*48
-  X2=XYZ2
-  Y2=X2+int(f[1])*48
-  Z2=Y2+int(f[1])*48
-  X3=XYZout
-  Y3=X3+int(f[1])*48
-  Z3=Y3+int(f[1])*48
-  """
-  ZZ1 = Z1^2
-  ZZZ1 = Z1*ZZ1
-  ZZ2 = Z2^2
-  ZZZ2 = Z2*ZZ2
-  A = X1*ZZ2
-  B = X2*ZZ1-A
-  c = Y1*ZZZ2
-  d = Y2*ZZZ1-c
-  e = B^2
-  f = B*e
-  g = A*e
-  h = Z1*Z2
-  f2g = 2*g+f
-  X3 = d^2-f2g
-  Z3 = B*h
-  gx = g-X3
-  Y3 = d*gx-c*f
-  """
-  # temp vars
-  ZZ1 = buffer_Eadd
-  ZZZ1 = ZZ1+int(f[1])*48
-  ZZ2 = ZZZ1+int(f[1])*48
-  ZZZ2 = ZZ2+int(f[1])*48
-  A = ZZZ2+int(f[1])*48
-  B = A+int(f[1])*48
-  c = B+int(f[1])*48
-  d = c+int(f[1])*48
-  e = d+int(f[1])*48
-  f_ = e+int(f[1])*48
-  g = f_+int(f[1])*48
-  h = g+int(f[1])*48
-  f2g = h+int(f[1])*48
-  gx = f2g+int(f[1])*48
-
-  print("ZZ1 = Z1^2")
-  gen_fmul(f,ZZ1,Z1,Z1,mod)
-  print("ZZZ1 = Z1*ZZ1")
-  gen_fmul(f,ZZZ1,Z1,ZZ1,mod)
-  print("ZZ2 = Z2^2")
-  gen_fmul(f,ZZ2,Z2,Z2,mod)
-  print("ZZZ2 = Z2*ZZ2")
-  gen_fmul(f,ZZZ2,Z2,ZZ2,mod)
-  print("A = X1*ZZ2")
-  gen_fmul(f,A,X1,ZZ2,mod)
-  print("B = X2*ZZ1-A")
-  gen_fmul(f,B,X2,ZZ1,mod)
-  gen_fsub(f,B,B,A,mod)
-  print("c = Y1*ZZZ2")
-  gen_fmul(f,c,Y1,ZZZ2,mod)
-  print("d = Y2*ZZZ1-c")
-  gen_fmul(f,d,Y2,ZZZ1,mod)
-  gen_fsub(f,d,d,c,mod)
-  print("e = B^2")
-  gen_fmul(f,e,B,B,mod)
-  print("f = B*e")
-  gen_fmul(f,f_,B,e,mod)
-  print("g = A*e")
-  gen_fmul(f,g,A,e,mod)
-  print("h = Z1*Z2")
-  gen_fmul(f,h,Z1,Z2,mod)
-  print("f2g = 2*g+f")
-  gen_fadd(f,f2g,g,g,mod)
-  gen_fadd(f,f2g,f2g,f_,mod)
-  print("X3 = d^2-f2g")
-  gen_fmul(f,X3,d,d,mod)
-  gen_fsub(f,X3,X3,f2g,mod)
-  print("Z3 = B*h")
-  gen_fmul(f,Z3,B,h,mod)
-  print("gx = g-X3")
-  gen_fsub(f,gx,g,X3,mod)
-  print("Y3 = d*gx-c*f")
-  gen_fmul(f,Y3,d,g,mod)
-  gen_fmul(f,c,c,f_,mod)	# clobber c
-  gen_fsub(f,Y3,Y3,c,mod)
-
-  print("// E add")
-  print("/////////")
-
 def gen_Eadd__madd_2007_bl(f,XYZout,XYZ1,XYZ2,line1,mod):
   print("/////////")
   print("// Eadd https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#addition-madd-2007-bl")
@@ -1231,7 +1137,6 @@ def gen_Edouble__dbl_2009_alnr(f,XYZout,XYZ,line0,mod):
   X3=XYZout
   Y3=X3+int(f[1])*48
   Z3=Y3+int(f[1])*48
-  #print("gen_Edouble__dbl_2009_alnr(",X1,Y1,Z1,X3,Y3,Z3,")")
 
   """
   A = X1^2
@@ -1292,75 +1197,10 @@ def gen_Edouble__dbl_2009_alnr(f,XYZout,XYZ,line0,mod):
   gen_fadd(f,C,C,C,mod)
   gen_fadd(f,C,C,C,mod)
   gen_fsub(f,Y3,Y3,C,mod)
-  """
-  """
+
   print("// E double")
   print("////////////")
   return A,B,E,F,ZZ,X1
-
-def gen_Edouble__dbl_2009_l(f,XYZout,XYZ,mod):
-  # this is untested
-  print("///////////")
-  print("// Edouble https://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#doubling-dbl-2009-l")
-
-  # inputs/ouput
-  X1=XYZ
-  Y1=X1+int(f[1])*48
-  Z1=Y1+int(f[1])*48
-  X3=XYZout
-  Y3=X3+int(f[1])*48
-  Z3=Y3+int(f[1])*48
-
-  """
-  A = X1^2
-  B = Y1^2
-  C = B^2
-  D = 2*((X1+B)^2-A-C)
-  E = 3*A
-  F = E^2
-  X3 = F-2*D
-  Y3 = E*(D-X3)-8*C
-  Z3 = 2*Y1*Z1
-  """
-  A = buffer_Edouble
-  B = A+int(f[1])*48
-  C = B+int(f[1])*48
-  D = C+int(f[1])*48
-  E = D+int(f[1])*48
-  F = E+int(f[1])*48
-
-  print("// A = X1^2")
-  gen_fmul(f,A,X1,X1,mod)
-  print("// B = Y1^2")
-  gen_fmul(f,B,Y1,Y1,mod)
-  print("// C = B^2")
-  gen_fmul(f,C,B,B,mod)
-  print("// D = 2*((X1+B)^2-A-C)")
-  gen_fadd(f,D,X1,B,mod)
-  gen_fmul(f,D,D,D,mod)
-  gen_fsub(f,D,D,A,mod)
-  gen_fsub(f,D,D,C,mod)
-  gen_fadd(f,D,D,D,mod)
-  print("// E = 3*A")
-  gen_fadd(f,F,A,A,mod)
-  gen_fadd(f,F,F,A,mod)
-  print("// F = E^2")
-  gen_fmul(f,F,E,E,mod)
-  print("// X3 = F-2*D")
-  gen_fadd(f,X3,D,D,mod)
-  gen_fsub(f,X3,F,D,mod)
-  print("// Y3 = E*(D-X3)-8*C")
-  gen_fsub(f,Y3,D,X3,mod)
-  gen_fmul(f,Y3,E,Y3,mod)
-  gen_fadd(f,C,C,C,mod)		# clobber C
-  gen_fadd(f,C,C,C,mod)
-  gen_fadd(f,C,C,C,mod)
-  gen_fsub(f,Y3,Y3,C,mod)
-  print("// Z3 = 2*Y1*Z1")
-  gen_fmul(f,Z3,Y1,Z1,mod)
-  gen_fadd(f,Z3,Z3,Z3,mod)
-  print("// E double")
-  print("////////////")
 
 # double a curve point
 #######################
@@ -1375,71 +1215,6 @@ def gen_Edouble__dbl_2009_l(f,XYZout,XYZ,mod):
 
 ##############
 # Miller Loop
-
-# hard-coded miller loop inputs, optional
-# these inputs can be hard-coded anywhere before the miller loop starts, we copy/paste the output of this function into main.huff
-def gen_miller_loop_test_input():
-  case = 3
-  if case==0:
-    # test from https://tools.ietf.org/id/draft-yonezawa-pairing-friendly-curves-02.html#rfc.appendix.B
-    # Input x,y values:
-    inE1  = bytearray.fromhex("120177419e0bfb75edce6ecc21dbf440f0ae6acdf3d0e747154f95c7143ba1c17817fc679976fff55cb38790fd530c16")[::-1]
-    inE1  += bytearray.fromhex("0e44d2ede97744303cff1b76964b531712caf35ba344c12a89d7738d9fa9d05592899ce4383b0270ff526c2af318883a")[::-1]
-    gen_memstore(buffer_inputs,inE1)
-    # Input x’0,x'1 value:
-    inE2 = bytearray.fromhex("058191924350bcd76f67b7631863366b9894999d1a3caee9a1a893b53e2ae580b3f5fb2687b4961af5f28fa202940a10")[::-1] 
-    inE2 += bytearray.fromhex("11922a097360edf3c2b6ed0ef21585471b1ab6cc8541b3673bb17e18e2867806aaa0c59dbccd60c3a5a9c0759e23f606")[::-1] 
-    # Input y’0,y'1 value:
-    inE2 += bytearray.fromhex("197d145bbaff0bb54347fe40525c8734a887959b8577c95f7f4a4d344ca692c9c52f05df531d63a56d8bf5079fb65e61")[::-1] 
-    inE2 += bytearray.fromhex("0ed54f48d5a1caa764044f659f0ee1e9eb2def362a476f84e0832636bacc0a840601d8f4863f9e230c3e036d209afa4e")[::-1] 
-    gen_memstore(buffer_inputs+96,inE2)
-  elif case==2:
-    print()
-    # these are the identity elements, copied from https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2539.md#specification
-    # G1:
-    inE1  = bytearray.fromhex("008848defe740a67c8fc6225bf87ff5485951e2caa9d41bb188282c8bd37cb5cd5481512ffcd394eeab9b16eb21be9ef")[::-1]
-    inE1  += bytearray.fromhex("01914a69c5102eff1f674f5d30afeec4bd7fb348ca3e52d96d182ad44fb82305c2fe3d3634a9591afd82de55559c8ea6")[::-1]
-    gen_memstore(buffer_inputs,inE1)
-    # G2:
-    inE2 = bytearray.fromhex("018480be71c785fec89630a2a3841d01c565f071203e50317ea501f557db6b9b71889f52bb53540274e3e48f7c005196")[::-1] 
-    inE2 += bytearray.fromhex("00ea6040e700403170dc5a51b1b140d5532777ee6651cecbe7223ece0799c9de5cf89984bff76fe6b26bfefa6ea16afe")[::-1] 
-    inE2 += bytearray.fromhex("00690d665d446f7bd960736bcbb2efb4de03ed7274b49a58e458c282f832d204f2cf88886d8c7c2ef094094409fd4ddf")[::-1] 
-    inE2 += bytearray.fromhex("00f8169fd28355189e549da3151a70aa61ef11ac3d591bf12463b01acee304c24279b83f5e52270bd9a1cdd185eb8f93")[::-1] 
-    gen_memstore(buffer_inputs+96,inE2)
-  elif case==3:
-    print()
-    # these are from wasmsnark
-    # cd wasmsnark && ~/repos/node/node-v12.18.4-linux-x64/bin/npx mocha test/bls12381.js
-    # G1:
-    inE1  = bytearray.fromhex("0f81da25ecf1c84b577fefbedd61077a81dc43b00304015b2b596ab67f00e41c86bb00ebd0f90d4b125eb0539891aeed")[::-1]
-    inE1  += bytearray.fromhex("11af629591ec86916d6ce37877b743fe209a3af61147996c1df7fd1c47b03181cd806fd31c3071b739e4deb234bd9e19")[::-1]
-    gen_memstore(buffer_inputs,inE1)
-    # G2:
-    inE2 = bytearray.fromhex("024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8")[::-1] 
-    inE2 += bytearray.fromhex("13e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e")[::-1] 
-    inE2 += bytearray.fromhex("0ce5d527727d6e118cc9cdc6da2e351aadfd9baa8cbdd3a76d429a695160d12c923ac9cc3baca289e193548608b82801")[::-1] 
-    inE2 += bytearray.fromhex("0606c4a02ea734cc32acd2b02bc28b99cb3e287e85a763af267492ab572e99ab3f370d275cec1da1aaa9075ff05f79be")[::-1] 
-    gen_memstore(buffer_inputs+96,inE2)
-  elif case==4:
-    # from https://datatracker.ietf.org/doc/draft-irtf-cfrg-pairing-friendly-curves/?include_text=1 appendix B
-    inE1  = bytearray.fromhex("17f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb")[::-1]
-    inE1  += bytearray.fromhex("08b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e1")[::-1]
-    gen_memstore(buffer_inputs,inE1)
-    inE2  = bytearray.fromhex("024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8")[::-1]
-    inE2  += bytearray.fromhex("13e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e")[::-1]
-    inE2  += bytearray.fromhex("0ce5d527727d6e118cc9cdc6da2e351aadfd9baa8cbdd3a76d429a695160d12c923ac9cc3baca289e193548608b82801")[::-1]
-    inE2  += bytearray.fromhex("0606c4a02ea734cc32acd2b02bc28b99cb3e287e85a763af267492ab572e99ab3f370d275cec1da1aaa9075ff05f79be")[::-1]
-    gen_memstore(buffer_inputs+96,inE2)
-  elif case==5:
-    # from casey
-    inE1  = bytearray.fromhex("0b83dfefb120fab7665a607d749ef1765fbb3cc0ba5827a20a135402c09d987c701ddb5b60f0f5495026817e8ab6ea2e")[::-1]
-    inE1  += bytearray.fromhex("15c82e5362493d173e96edb436e396a30b9d3ae5d1a2633c375cfbbf3aed34bbc30448ec6b8102ab2f8da4486d23a717")[::-1]
-    gen_memstore(buffer_inputs,inE1)
-    inE2  = bytearray.fromhex("16fc2f7ff7eb01f34e97a5d5274390ee168f32ff5803597da434b40fa7778793eaac8cc3e8f0d75f3bf55889258ebea7")[::-1]
-    inE2  += bytearray.fromhex("183aa5f5b84721a4efdfc5a759ec88792e3080b8f9207d02eca66082d6076569b84b95e05b3a4b95697909f1dda69d8d")[::-1]
-    inE2  += bytearray.fromhex("002e5c809b03e98d5406ae13e3aa6e477b4aa0a0cedef70dafdd5f0b0c2c64152f52837f92870d0c57b21dd62e9ead91")[::-1]
-    inE2  += bytearray.fromhex("039dc3bb023f737d7c60f62b4e669843817fe1ed0751a7b750d02c9df5ee87758e7fe7d6fd614b5fe013f35e6fd9ae4d")[::-1]
-    gen_memstore(buffer_inputs+96,inE2)
 
 def gen_line_add(line,T,R,Q,mod):
   # line is 3 f2s, T on E2, R on E2, Q on E2 affine
@@ -1581,29 +1356,6 @@ The two biggest pieces of bytecode are f12mul and f12sqrcyclotomic. So we have o
 Function calls operate on hard-coded memory offsets, so there may be memcopy input/output overhead. We try to minimize this memcopying by conveniently having values already there when possible.
 """
 
-def gen_final_exp_test_input():
-  # this is to generate test values, which are then hard-coded into the main.huff
-  case = 3
-  if case==1:
-    pass
-  elif case==2:
-    pass
-  elif case==3:
-    # from test 3
-    a  = bytearray.fromhex("3cf8fe6aba6061de70638196991c6c245ccc8d011108841a2d19eecc877c3fbcb1cc9a694c0aac2903c8c41520526a00")
-    a += bytearray.fromhex("16a3242ef581eb5bc185108cbde497bc21150576fd77b2c9166fb392a444e0503c54caed9cba054d3851c6fd58821917")
-    a += bytearray.fromhex("1d364d5982bdece58c3d9ff500fd25ae4589ca4d3c2d81391ce1a3afbc4c5ffaab909e5e2f03b4e690a4b7677dd39102")
-    a += bytearray.fromhex("a5a6a20f4e3d0702b4dc8f8dab2dbd3e5b769c5116bc3889502ced4f6efd1f5e344b1f6299bb1e7c9508f465a64da515")
-    a += bytearray.fromhex("e9f8ce46f6dd5883b4342c56af2ffcf89088ccd992bb4b707824d3ad74d569b07f9ce69c0c7c38907ff41b57c0ce190c")
-    a += bytearray.fromhex("925dd857f1ef631f083592773a02acb2c93e1dd2940cf0d29bed0d3710c2128811267eba6d19202807de48bb42699108")
-    a += bytearray.fromhex("2d0bc7f84f68520c8cd47622db80eb306e1dc84ba817c63539ac4455c4284efcb18301782b2100096e01840ae3dfe302")
-    a += bytearray.fromhex("7f67af26013c4bbf335e878f66312c5c645fb5418ba855c6bac3324fbda16158c4c87246767192c372029e9e9b0c3b0c")
-    a += bytearray.fromhex("2c57f559fdd03dc9a9443633bf4a1f0b5306341fbf67d47ef5bef488a82f5adb75e860182004899f9ad9ad48a5c36d08")
-    a += bytearray.fromhex("b2e7a310546da097a4f53d663c875fea9cf99e7e6a1c607d23004c368f1f4955c00c2352f10484e582115d83cb5cd019")
-    a += bytearray.fromhex("f6aed3544c28c8b9efda117c0b8241adc55ed0ee88c6bce0a06f41a11a1ddb939ddba145345d9e78854c554554357414")
-    a += bytearray.fromhex("15a66fbe62fa646a72f15b3ffdccdae69abfc78c290cebb24fbb96b781375f87fbdb4168ccc04f245f02531b38e1400a")
-    gen_memstore(buffer_miller_output,a)
-
 def gen_f12raise_to_z_with_function_calls(out,x,mod):
   if x!=buffer_f12_function2:
     gen_memcopy(buffer_f12_function2,x,48*12)
@@ -1684,6 +1436,9 @@ def gen_f12sqrcyclotomic_loop_function(out,x,mod):
   print("jump")	# jump to returndest
  
 def gen_final_exponentiation_with_function_calls(out,in_,mod):
+  # this function uses function calls, but bytecode is still too large
+  # we keep this function here in case we have to debug gen_final_exponentiation_with_function_calls_optimized_mem_locations()
+  #                                    or we change the EVM384 interface and this function is no longer too large
   y0 = buffer_finalexp
   y1 = y0+12*48
   y2 = y1+12*48
@@ -1724,9 +1479,13 @@ def gen_final_exponentiation_with_function_calls(out,in_,mod):
   gen_f12frobeniusmap(y2,y3,1,mod)
   gen_f12mul_with_function_call(out,y1,y2,mod)
 
+  print("final_exp_done jump")
+
   # generate the actual functions which we "call"
   gen_f12mul_function(buffer_f12_function,buffer_f12_function2,mod)
   gen_f12sqrcyclotomic_loop_function(buffer_f12_function,buffer_f12_function,mod)
+
+  print("final_exp_done:")
 
 def gen_final_exponentiation_with_function_calls_optimized_mem_locations(out,in_,mod):
   y0 = buffer_finalexp
@@ -1820,46 +1579,22 @@ def gen_consts(miller_loop_flag):
 def gen_pairing():
   print("#include \"inversemod_bls12381.huff\"")
 
-  # init memory with consts like the modulus
+  # init memory with consts like the modulus, this is required
   print("#define macro INIT_MEM = takes(0) returns(0) {")
   gen_consts(1)	
   print("} // INIT_MEM")
 
-  # these are just some hard-coded inputs which may be useful for testing
-  print("#define macro MILLER_LOOP_TEST_VALUES = takes(0) returns(0) {")
-  gen_miller_loop_test_input()	# hard-code values for testing
-  print("} // MILLER_LOOP_TEST_VALUES")
-  print("#define macro FINAL_EXPONENTIATION_TEST_VALUES = takes(0) returns(0) {")
-  gen_final_exp_test_input()	# hard-code values for testing
-  print("} // FINAL_EXPONENTIATION_TEST_VALUES")
-
   # miller loop macro
   print("#define macro MILLER_LOOP = takes(0) returns(0) {")
-  #gen_consts(1)			# consts like the modulus, this is required
-  gen_miller_loop(buffer_miller_output,buffer_inputs,buffer_inputs+96,mod)
+  #gen_miller_loop(buffer_miller_output,buffer_inputs,buffer_inputs+96,mod)
+  gen_miller_loop(0,buffer_inputs,buffer_inputs+96,mod)
+  #gen_miller_loop(buffer_miller_output,0,96,mod)
   print("} // MILLER_LOOP")
 
   # final exponentiation macro
   print("#define macro FINAL_EXPONENTIATION = takes(0) returns(0) {")
-  gen_final_exponentiation_with_function_calls_optimized_mem_locations(buffer_finalexp_output,buffer_miller_output,mod)
+  gen_final_exponentiation_with_function_calls_optimized_mem_locations(0,0,mod)
   print("} // FINAL_EXPONENTIATION")
-
-  # pairing equation with two pairings, using the multi-pairing trick so only one final exponentiation
-  # this is untested, but it has two miller loops, a f2mul, a final exponentiation, and an equality check
-  print("#define macro PAIRING_EQ2 = takes(0) returns(0) {")
-  # first miller loop
-  gen_miller_loop(buffer_miller_output,buffer_inputs,buffer_inputs+96,mod)
-  gen_memcopy(buffer_f12_function2,buffer_miller_output,48*12)
-  # second miller loop
-  gen_miller_loop(buffer_miller_output,buffer_inputs,buffer_inputs+96,mod)
-  gen_memcopy(buffer_f12_function,buffer_miller_output,48*12)
-  # multiply the two miller loop outputs
-  gen_f12mul_with_function_call(buffer_f12_function,buffer_f12_function,buffer_f12_function2,mod)
-  # final exp
-  gen_final_exponentiation_with_function_calls_optimized_mem_locations(buffer_finalexp_output,buffer_f12_function,mod)
-  # check if equals one
-  gen_equals(f12one,buffer_finalexp_output,12*48)
-  print("} // PAIRING_EQ2")
 
 # the main generators
 ######################
@@ -1967,20 +1702,12 @@ def gen_final_exponentiation_unrolled(out,in_,mod):
 def gen_pairing_unrolled():
   # not sure if these are correct anymore, but the worked at some point
 
-  print("#include \"inversemod_bls12381.huff\"")
+  print("#include \"inversemod/inversemod_bls12381.huff\"")
 
   # generate huff macro to initialize memory
   print("#define macro INIT_MEM = takes(0) returns(0) {")
   gen_consts(1)			# consts like the modulus, this is required
   print("} // INIT_MEM")
-
-  # these are just some hard-coded inputs which may be useful for testing
-  print("#define macro FINAL_EXPONENTIATION_TEST_VALUES = takes(0) returns(0) {")
-  gen_final_exp_test_input()	# hard-code values for testing
-  print("} // FINAL_EXPONENTIATION_TEST_VALUES")
-  print("#define macro MILLER_LOOP_TEST_VALUES = takes(0) returns(0) {")
-  gen_miller_loop_test_input()	# hard-code values for testing
-  print("} // MILLER_LOOP_TEST_VALUES")
 
   print("#define macro MILLER_LOOP = takes(0) returns(0) {")
   gen_miller_loop_unrolled(buffer_miller_output,buffer_inputs,buffer_inputs+96,mod)
